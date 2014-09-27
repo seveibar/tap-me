192202016ebd4c0dcffd123bc0c9887a9948cdeb
Aaron Gunderson <aaron@agundy.com>
2014-09-27 19:58:47 -0400

    Basic server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,38 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var server = require("http").createServer(app);
+
+var clients = {};
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send('Welcome to Tap Me.')
+});
+
+app.get('/tap/:token', function (req,res) {
+    var token = req.param("token");
+    clients[token] = true;
+    res.json({
+        message: "Set to True"
+    });
+});
+
+app.get('/poll/:token', function (req,res) {
+    var token = req.param("token");
+    if(clients[token]){
+        res.json({
+            message: "Notify!"
+        });
+    }
+    else{
+        res.json({
+            message: "Wait"
+        });
+    }
+    clients[token] = false;
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
