5530a5e4c24e2d62ba94d0c934fc5b3fc063fd6c
Aaron Gunderson <aaron@agundy.com>
2014-09-28 02:57:12 -0400

    Made underline follow accross sections in menu bar

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,11 +1,33 @@
-$("#home").click( function () {
-    $("#home-div").addClass("hidden");
+$("#tap-me").click( function () {
+    $("#home").removeClass("active");
+    $("#about").removeClass("active");
+    $("#preorder").removeClass("active");
+    $("#home").addClass("active");
+
+    $("#home-div").addClass("hidden").animate();
     $("#home-div").toggleClass("hidden");
     $("#about-div").addClass("hidden");
     $("#preorder-div").addClass("hidden");
-
 });
+
+$("#home").click( function () {
+    $("#home").removeClass("active");
+    $("#about").removeClass("active");
+    $("#preorder").removeClass("active");
+    $("#home").addClass("active");
+
+    $("#home-div").addClass("hidden").animate();
+    $("#home-div").toggleClass("hidden");
+    $("#about-div").addClass("hidden");
+    $("#preorder-div").addClass("hidden");
+});
+
 $("#about").click( function () {
+    $("#home").removeClass("active");
+    $("#about").removeClass("active");
+    $("#preorder").removeClass("active");
+    $("#about").addClass("active");
+
     $("#about-div").addClass("hidden");
     $("#about-div").toggleClass("hidden");
     $("#home-div").addClass("hidden");
@@ -13,6 +35,11 @@ $("#about").click( function () {
 });
 
 $("#preorder").click( function () {
+    $("#home").removeClass("active");
+    $("#about").removeClass("active");
+    $("#preorder").removeClass("active");
+    $("#preorder").addClass("active");
+
     $("#home-div").addClass("hidden");
     $("#about-div").addClass("hidden");
     $("#preorder-div").addClass("hidden");
