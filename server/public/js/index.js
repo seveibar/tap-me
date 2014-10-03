8687f4ad7b86aa2a9123e65aab6639c68f732afe
Aaron Gunderson <aaron@agundy.com>
2014-10-03 01:13:20 -0400

    Working on taking emails for updates

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,47 +1,77 @@
+var home = $("#home");
+var about = $("#about");
+var preorder = $("#preorder");
+
+var home_div = $("#home-div");
+var about_div = $("#about-div");
+var preorder_div = $("#preorder-div");
+
 $("#tap-me").click( function () {
-    $("#home").removeClass("active");
-    $("#about").removeClass("active");
-    $("#preorder").removeClass("active");
-    $("#home").addClass("active");
+    home.removeClass("active");
+    about.removeClass("active");
+    preorder.removeClass("active");
+    home.addClass("active");
 
-    $("#home-div").addClass("hidden").animate();
-    $("#home-div").toggleClass("hidden");
-    $("#about-div").addClass("hidden");
-    $("#preorder-div").addClass("hidden");
+    home_div.addClass("hidden").animate();
+    home_div.toggleClass("hidden");
+    about_div.addClass("hidden");
+    preorder_div.addClass("hidden");
 });
 
-$("#home").click( function () {
-    $("#home").removeClass("active");
-    $("#about").removeClass("active");
-    $("#preorder").removeClass("active");
-    $("#home").addClass("active");
+home.click( function () {
+    home.removeClass("active");
+    about.removeClass("active");
+    preorder.removeClass("active");
+    home.addClass("active");
 
-    $("#home-div").addClass("hidden").animate();
-    $("#home-div").toggleClass("hidden");
-    $("#about-div").addClass("hidden");
-    $("#preorder-div").addClass("hidden");
+    home_div.addClass("hidden").animate();
+    home_div.toggleClass("hidden");
+    about_div.addClass("hidden");
+    preorder_div.addClass("hidden");
 });
 
-$("#about").click( function () {
-    $("#home").removeClass("active");
-    $("#about").removeClass("active");
-    $("#preorder").removeClass("active");
-    $("#about").addClass("active");
+about.click( function () {
+    home.removeClass("active");
+    about.removeClass("active");
+    preorder.removeClass("active");
+    about.addClass("active");
 
-    $("#about-div").addClass("hidden");
-    $("#about-div").toggleClass("hidden");
-    $("#home-div").addClass("hidden");
-    $("#preorder-div").addClass("hidden");
+    about_div.addClass("hidden");
+    about_div.toggleClass("hidden");
+    home_div.addClass("hidden");
+    preorder_div.addClass("hidden");
 });
 
-$("#preorder").click( function () {
-    $("#home").removeClass("active");
-    $("#about").removeClass("active");
-    $("#preorder").removeClass("active");
-    $("#preorder").addClass("active");
+preorder.click( function () {
+    home.removeClass("active");
+    about.removeClass("active");
+    preorder.removeClass("active");
+    preorder.addClass("active");
 
-    $("#home-div").addClass("hidden");
-    $("#about-div").addClass("hidden");
-    $("#preorder-div").addClass("hidden");
-    $("#preorder-div").toggleClass("hidden");
+    home_div.addClass("hidden");
+    about_div.addClass("hidden");
+    preorder_div.addClass("hidden");
+    preorder_div.toggleClass("hidden");
 });
+var show_preorder = function () {
+
+};
+
+var submit = function(){
+    var email = $("#email").val();
+    var data = {
+        email: email
+    };
+    var re = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    console.log(re.test(email));
+    if(re.test(email)){
+        $.post('/subscribe',data).done(
+            function(data){
+                console.log("Status is: " + data["status"]);
+        });
+    } else{
+        console.log(email + " isn't valid");
+        email
+    }
+
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var server = require("http").createServer(app);
+var fs = require('fs');
+var validator = require("validator");
+
+var email_file = __dirname + '/subscribers.json';
+
+// Set up middleware for parsing post requests
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var clients = {};
 
@@ -9,6 +17,31 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Welcome to Tap Me.')
+});
+
+app.post('/subscribe', function(req,res){
+    var email = validator.toString(req.body.email);
+    var ipaddress = req.connection.remoteAddress;
+    var date = new Date();
+    console.log(email);
+    var signup_list = {};
+
+    fs.readFile(email_file,'utf-8', function(err,data){
+        if(err){
+            console.log('Error: ' + err);
+            return;
+        }
+        signup_list = JSON.parse(data);
+        console.log(signup_list);
+        signup_list["subscribers"].push({
+            email: email,
+            ip: ipaddress,
+            datetime: date
+        });
+        console.log(signup_list);
+        fs.writeFile("subscribers.json", JSON.stringify(signup_list), "UTF-8");
+        res.json({status:"Good"});
+    });
 });
 
 app.get('/tap/:token', function (req,res) {
